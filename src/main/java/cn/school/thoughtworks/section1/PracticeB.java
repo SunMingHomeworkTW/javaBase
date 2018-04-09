84fc20caa408a7e26c8def47be0ab8f1049486db
SunMing <1229536801@qq.com>
2018-04-09 12:45:13 +0800

    完成section1/PracticeB，实现功能：选出1集合中与2集合中子数组的元素相同的元素

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> sameElements=new ArrayList<>();
+        for(String str:collection1){
+            for(List list:collection2){
+                if(list.contains(str))
+                    sameElements.add(str);
+            }
+        }
+        return sameElements;
     }
 }
