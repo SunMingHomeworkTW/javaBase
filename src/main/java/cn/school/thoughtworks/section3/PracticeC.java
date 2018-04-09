4dc402383a7f7fdc4452afac011ee67b59ba7ed1
SunMing <1229536801@qq.com>
2018-04-09 14:44:53 +0800

    完成section3/PracticeC，实现功能：统计出collection1中相同的元素的个数，形成collection3，collection3是一个map其中元素key是collection1中的元素value是该元素在collection1中出现次数，然后选出collection3跟collection2中value属性对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,34 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+  Map<String, Integer> createUpdatedCollection(List<String> collectionA,
+      Map<String, List<String>> object) {
+    //实现练习要求，并改写该行代码。
+    //统计集合collectionA中重复元素出现的次数
+    Map<String, Integer> collection3 = new HashMap<>();
+    int num;
+    for (String str : collectionA) {
+      if (collection3.containsKey(str)) {
+        num = collection3.get(str) + 1;
+      } else {
+        num = 1;
+      }
+      collection3.put(str, num);
     }
+
+    //选出collection3跟object中value属性对应的list中的元素相同的元素,把他们的count，满3减1
+    for (String str : collection3.keySet()) {
+      for (List list : object.values()) {
+        if (list.contains(str)) {
+          collection3.replace(str, collection3.get(str) - collection3.get(str) / 3);
+        }
+      }
+    }
+    return collection3;
+  }
 }
