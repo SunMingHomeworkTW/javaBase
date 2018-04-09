e7954f1be2d75c15f97e61a405839ae1caeed40d
SunMing <1229536801@qq.com>
2018-04-09 12:51:19 +0800

    完成section1/Practicec，实现功能：选出1集合中跟2对象中value属性中的元素相同的元素

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,19 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> sameElements=new ArrayList<>();
+        for(String str:collection1){
+            for(List list:collection2.values()){
+                if (list.contains(str))
+                    sameElements.add(str);
+            }
+        }
+        return sameElements;
     }
 }
