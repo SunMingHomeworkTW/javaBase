bee32a69366fecd235f704b3a2223f8038a1a5fa
SunMing <1229536801@qq.com>
2018-04-09 14:49:30 +0800

    完成section3/PracticeD，实现功能：统计出collection1中相同的元素的个数，有过有-就把-右边的数字也计算入个数，形成collection3，collection3是一个map其中元素key是collection1中的元素value是该元素在collection1中出现次数，然后选出collection3中的元素的key属性跟collection2中value属性对应的list中的元素相同的元素,把他们的count，满3减1，输出减过之后的新集合

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,42 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+  Map<String, Integer> createUpdatedCollection(List<String> collectionA,
+      Map<String, List<String>> object) {
+    //实现练习要求，并改写该行代码。
+    //统计集合collectionA中重复元素出现的次数,有过有-就把-右边的数字也计算入个数
+    Map<String, Integer> collection3 = new HashMap<>();
+    int num;
+    int num1;
+    for (String str : collectionA) {
+      if (str.indexOf("-") > 0) {
+        String[] strs = str.split("-");
+        str = strs[0];
+        num1 = Integer.valueOf(strs[1]);
+      } else {
+        num1 = 1;
+      }
+      if (collection3.containsKey(str)) {
+        num = collection3.get(str) + num1;
+      } else {
+        num = num1;
+      }
+      collection3.put(str, num);
     }
+
+    //选出collection3跟object中value属性对应的list中的元素相同的元素,把他们的count，满3减1
+    for (String str : collection3.keySet()) {
+      for (List list : object.values()) {
+        if (list.contains(str)) {
+          collection3.replace(str, collection3.get(str) - collection3.get(str) / 3);
+        }
+      }
+    }
+    return collection3;
+  }
 }
