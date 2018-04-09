47e7366e0135c803b8bbeea8223e41cb1aba9984
SunMing <1229536801@qq.com>
2018-04-09 13:43:44 +0800

    完成section2/PracticeA，实现功能：把1集合中相同的元素统计出数量

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,23 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+  Map<String, Integer> countSameElements(List<String> collection1) {
+    //实现练习要求，并改写该行代码。
+    Map<String, Integer> sameElementsNum = new HashMap<>();
+    int num;
+    for (String str : collection1) {
+      if (sameElementsNum.containsKey(str)) {
+        num = sameElementsNum.get(str) + 1;
+      } else {
+        num = 1;
+      }
+      sameElementsNum.put(str, num);
     }
+    return sameElementsNum;
+  }
 }
